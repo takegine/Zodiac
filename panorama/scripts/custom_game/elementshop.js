fe56d095d52535ea23f93aab863dff819de01606
takegine <405773654@qq.com>
2020-06-24 13:40:01 +0800

    整理代码

--- panorama/scripts/custom_game/elementshop.js
+++ panorama/scripts/custom_game/elementshop.js
@@ -1,72 +1,71 @@
 var crafts = [
-         [4,8,2],
-
-            [8,1,4],    
-
-                [9,5,6],     
-    [10,3,2],   
-       [6,8,7],
+    [4,8,2],
+    [8,1,4],
+    [9,5,6],
+    [10,3,2],
+    [6,8,7],
     [6,5,2],
-    [7,2,4],     
-    [10,1,6],  
-        [8,5,3],
-    [6,8,1],     
-      [9,8,2],    
-    [2,5,10],    
-    [3,4,2],    
-    [7,3,10],    
+    [7,2,4],
+    [10,1,6],
+    [8,5,3],
+    [6,8,1],
+    [9,8,2],
+    [2,5,10],
+    [3,4,2],
+    [7,3,10],
     [10,6,4],
     [3,7,9],
     [9,1,10],
-    [9,3,8],    
-  [7,1,5],
-    [9,6,2],    
-    [4,7,1],    
-    [5,3,1],     
+    [9,3,8],
+    [7,1,5],
+    [9,6,2],
+    [4,7,1],
+    [5,3,1],
     [6,5,7],
     [7,1,9],
     [5,8,10],
-    [4,6,3],    
-    [2,1,3],    
+    [4,6,3],
+    [2,1,3],
     [4,9,5],
-    [10,7,8],    
-    [10,4,9] 
+    [10,7,8],
+    [10,4,9]
 
 ];
 
 var elems = ["item_life","item_water","item_shadow","item_void","item_earth","item_fire","item_light","item_air","item_ice","item_energy"];
 
-var items = ["item_air_void_water",
-"item_air_life_void",
-"item_ice_fire_earth",
-"item_energy_shadow_water",
-"item_air_fire_light",
-"item_fire_earth_water",
-"item_light_water_void",
-"item_energy_life_fire",
+var items = [
+    "item_air_void_water",
+    "item_air_life_void",
+    "item_ice_fire_earth",
+    "item_energy_shadow_water",
+    "item_air_fire_light",
+    "item_fire_earth_water",
+    "item_light_water_void",
+    "item_energy_life_fire",
     "item_air_earth_shadow",
-"item_air_fire_life",
- "item_ice_air_water",
-                "item_energy_earth_water",
-                "item_void_shadow_water",
-                "item_shadow_energy_light",
-                "item_energy_fire_void",
-                "item_shadow_light_ice",
-                "item_ice_life_energy",
-                "item_ice_shadow_air",
+    "item_air_fire_life",
+    "item_ice_air_water",
+    "item_energy_earth_water",
+    "item_void_shadow_water",
+    "item_shadow_energy_light",
+    "item_energy_fire_void",
+    "item_shadow_light_ice",
+    "item_ice_life_energy",
+    "item_ice_shadow_air",
     "item_light_life_earth",
-                "item_water_ice_fire",
-                "item_void_light_life",
-                "item_earth_shadow_life",
+    "item_water_ice_fire",
+    "item_void_light_life",
+    "item_earth_shadow_life",
     "item_light_fire_earth",
-                "item_light_life_ice",
-                "item_air_earth_energy",
-                "item_void_fire_shadow",
-                "item_water_life_shadow",
-                "item_void_ice_earth",
-                "item_energy_light_air",
-                "item_energy_void_ice"
-];
+    "item_light_life_ice",
+    "item_air_earth_energy",
+    "item_void_fire_shadow",
+    "item_water_life_shadow",
+    "item_void_ice_earth",
+    "item_energy_light_air",
+    "item_energy_void_ice"
+    ];
 
 
 function Yes()
@@ -77,8 +76,8 @@ function Yes()
             if ($("#ShopInfo").BHasClass("ShopInfoAnim"))
                 {
                  $("#ShopInfo").RemoveClass("ShopInfoAnim");
-                 $("#ShopInfo").AddClass("ShopInfoAnim");    }   
-                
+                 $("#ShopInfo").AddClass("ShopInfoAnim");    }
+
         //GameEvents.SendCustomGameEventToServer( "Levels", { id: Players.GetLocalPlayer()} );
         }
     $("#readytext").RemoveClass("ElementShopText");//移除闪烁特效
@@ -115,7 +114,7 @@ function UpdateShop( table_name, key, data )
             { $("#btntop"+(i-1)).RemoveClass("offcraft");
             if (data[i] == 0) {  $("#btntop"+(i-1)).AddClass("offcraft");  }
             if (data[i] != null) {  $("#lvlmyitemtext"+i).text = "x "+data[i];  }        }
-        
+
         for (var y = 1; y <= 30;y++)
             {
             $("#craft"+y).RemoveClass("offcraft");//移除灰色
@@ -134,7 +133,7 @@ function UpdateShop( table_name, key, data )
                         $("#craft"+y).AddClass("offcraft");
                         break;
                     }
-                    
+
                 }
             }
         }
@@ -144,7 +143,7 @@ function UpdateShop( table_name, key, data )
 (function()//立即执行的函数
 {
     CustomNetTables.SubscribeNetTableListener( "Elements_Tabel", UpdateShop );
-    
+
     $("#ShopInfo").visible = false;
     for (var i = 1; i <= 30;i++)
     {

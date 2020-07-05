8bcb6d531f5522f0153d2c599069cb34bbeb7050
takegine <405773654@qq.com>
2020-07-05 14:40:23 +0800

    第2关技能调整

--- scripts/vscripts/skill/02_my_str_steel.lua
+++ scripts/vscripts/skill/02_my_str_steel.lua
@@ -1,23 +1,45 @@
-LinkLuaModifier("my_str_debuff","modifiers/02_my_str_debuff", LUA_MODIFIER_MOTION_NONE)
+
 
 function OnAttack( event )
-    local caster = event.caster
-    local target = event.target
+    local caster  = event.caster
+    local target  = event.target
     local ability = event.ability
     
-    if target:FindModifierByName("my_str_debuff") == nil then
-        target:AddNewModifier(target,ability,"my_str_debuff",{duration = 30})
-        local modif = target:FindModifierByName("my_str_debuff")
+    local modif   = target:FindModifierByName("modifier_decrease_strength")
+
+    if not modif then
+
+        target:AddNewModifier(target, ability, "modifier_decrease_strength", {duration = 30})
         modif:IncrementStackCount()
-        for i =1,_G.hardmode do modif:IncrementStackCount() end
-        print("round 02 ,the modifier count equal the level of hardmode："..modif:GetStackCount().."now hardmode is:".._G.hardmode)
-   
     else
-        local modif = target:FindModifierByName("my_str_debuff")
         modif:SetDuration(30,true)
-        for i =1,_G.hardmode do modif:IncrementStackCount() end
-    	print("round 02 ,the modifier count equal the level of hardmode："..modif:GetStackCount(),"now hardmode is:".._G.hardmode)
-      
     end
     
+    for i =1,_G.hardmode do
+        modif:IncrementStackCount() 
+    end
+    print("round 02 ,the modifier count equal the level of hardmode："..modif:GetStackCount().."now hardmode is:".._G.hardmode)
+end
+
+LinkLuaModifier("modifier_decrease_strength","skill/02_my_str_steel", LUA_MODIFIER_MOTION_NONE)
+modifier_decrease_strength = class ({})
+
+function modifier_decrease_strength:GetTexture ()
+    return "undying_decay"
+end
+
+function modifier_decrease_strength:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS
+	}
+ 
+	return funcs
+end
+
+function modifier_decrease_strength:IsDebuff()
+	return true
+end
+
+function modifier_decrease_strength:GetModifierBonusStats_Strength( params )
+	return -self:GetStackCount()
 end

ffc873e8e2e2b2b293be0b4e12c28f309a2a140f
takegine <405773654@qq.com>
2020-07-13 16:14:08 +0800

    微调了一个装备的脚本

--- scripts/vscripts/modifiers/item_attack_re.lua
+++ scripts/vscripts/modifiers/item_attack_re.lua
@@ -3,11 +3,10 @@ function Attack( event )
 	local caster = event.caster
 	local ability	= event.ability
     if not caster:IsIllusion() then
-        if caster.att_target ~= nil then
-            if not caster.att_target:IsNull() then
-                if caster.att_target:IsAlive() then
+        if caster.att_target then
+            if not  caster.att_target:IsNull() 
+            and     caster.att_target:IsAlive() then
                     caster.att_target:RemoveModifierByName("modifier_item_void_fire_damage")
-                end
             end
         end
         ability:ApplyDataDrivenModifier( caster, target, "modifier_item_void_fire_damage", {} )
@@ -18,29 +17,27 @@ end
 -- Keeps track of the targets health
 function OnTakeDamage( event )
     --DeepPrintTable(event)
-	local damage = event.Damage
+	local damage = event.Damage * event.Perc / 100
 	local caster = event.caster
-	local ability	= event.ability
+	local ability= event.ability
     
-    if not caster:IsIllusion() then
-    if caster:GetAttackCapability() == 1 then
-    if caster.att_target ~= nil then
-    if caster.att_target ~= caster then
-    if damage * (event.Perc / 100) > 1 then
-	local nFXIndex = ParticleManager:CreateParticle( "particles/my_wraith_king_vampiric_aura_lifesteal.vpcf", PATTACH_ABSORIGIN, caster.att_target )
+    if caster:IsIllusion()
+    or caster:GetAttackCapability() ~= 1
+    or not caster.att_target
+    or caster.att_target == caster
+    or damage <= 1 
+    then return
+    end
+    local pfxName  = "particles/units/heroes/hero_skeletonking/wraith_king_vampiric_aura_lifesteal.vpcf"
+	local nFXIndex = ParticleManager:CreateParticle( pfxName, PATTACH_ABSORIGIN, caster.att_target )
 	ParticleManager:SetParticleControlEnt( nFXIndex, 1, caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetOrigin(), true )
 	local damageTable = {
-	victim = caster.att_target,
-	attacker = caster,
-	damage = damage * (event.Perc / 100),
-	damage_type = DAMAGE_TYPE_PURE,
-	damage_flags = DOTA_DAMAGE_FLAG_REFLECTION + DOTA_DAMAGE_FLAG_NO_SPELL_LIFESTEAL, --Optional.
-	ability = ability --Optional.
+        victim      = caster.att_target,
+        attacker    = caster,
+        damage      = damage,
+        damage_type = DAMAGE_TYPE_PURE,
+        damage_flags= DOTA_DAMAGE_FLAG_REFLECTION + DOTA_DAMAGE_FLAG_NO_SPELL_LIFESTEAL, --Optional.
+        ability     = ability --Optional.
     }
     ApplyDamage(damageTable)
-    end
-    end
-    end
-    end
-    end
 end

4e0188c4f125cccd5d83cb82897ff5f922a2daed
takegine <405773654@qq.com>
2020-07-13 14:35:35 +0800

    微调BOSS_hp_bar

--- panorama/scripts/custom_game/boss_hp.js
+++ panorama/scripts/custom_game/boss_hp.js
@@ -1,56 +1,37 @@
-function RefreshHPData(data){
+(function(){ 
 
-    var panleId=data.name
-    var panleSvalue=data.svalue
-    var panleEvalue=data.evalue
-    var remark=data.remark
-    
-    var questPanle=$('#QuestPanel').FindChild(panleId)
-    var valuePercent=parseInt(panleSvalue)/parseInt(panleEvalue)*100;
-    if(questPanle!=null){
-        sliderPanle=questPanle.GetChild(0);
-        sliderPanle.GetChild(0).style.width=valuePercent.toString()+"%"
-        sliderPanle.GetChild(1).style.width=(100-valuePercent).toString()+"%"
-        questPanle.GetChild(1).GetChild(0).text=panleText
-         if (remark!=null)
-         {
-             questPanle.GetChild(1).GetChild(0).SetDialogVariableInt( "remark", remark);
-         }
-         if (panleSvalue!=null)
-         {
-             questPanle.GetChild(1).GetChild(0).SetDialogVariableInt( "panleSvalue", panleSvalue);
-         }
-         if (panleEvalue!=null)
-         {
-             questPanle.GetChild(1).GetChild(0).SetDialogVariableInt( "panleEvalue", panleEvalue);
-         }
-         if (panleEvalue!=null)
-         {
-             if (panleSvalue!=null)
-            {
-                questPanle.GetChild(1).GetChild(0).SetDialogVariableInt( "Eva-Sva", panleEvalue - panleSvalue);
-            }
-         }
-        var panleText=$.Localize(data.text,questPanle.GetChild(1).GetChild(0))
-        questPanle.GetChild(1).GetChild(0).text=panleText
-    }
-}
-
-function CreatHP(data) {
+    GameEvents.Subscribe( "createhp", (data) => {
         newPanel = $.CreatePanel('Panel', $('#QuestPanel'),data.name);
         newPanel.BLoadLayoutSnippet("QuestLine");
         newPanel.AddClass("Panle_MarginStyle")
-}
+    });
 
-function RemoveHPPUI(data){
-    $.Msg(data.name+"data name")
-    var RemovePanle=$('#QuestPanel').FindChild(data.name)
-    RemovePanle.deleted = true;
-    RemovePanle.DeleteAsync(0);
-}
+    GameEvents.Subscribe( "removehppui", (data) => {
+        $.Msg(data.name+"data name")
+        var RemovePanle=$('#QuestPanel').FindChild(data.name)
+        RemovePanle.deleted = true;
+        RemovePanle.DeleteAsync(0);
+    });
 
-(function(){ 
-    GameEvents.Subscribe( "createhp", CreatHP);
-    GameEvents.Subscribe( "refreshhpdata", RefreshHPData);
-    GameEvents.Subscribe( "removehppui", RemoveHPPUI);
+    GameEvents.Subscribe( "refreshhpdata", (data) => {
+
+        var panleId    = data.name
+        var panleSvalue= data.svalue
+        var panleEvalue= data.evalue
+        var text       = data.text
+        
+        var questPanle = $('#QuestPanel').FindChild( panleId )
+        var HpPercent  = parseInt(panleSvalue) / parseInt(panleEvalue) *100;
+        
+        if( questPanle )
+            sliderPanle = questPanle.GetChild(0);
+            sliderPanle.GetChild(0).style.width = HpPercent.toString()+"%";
+            sliderPanle.GetChild(1).style.width = (100-HpPercent).toString()+"%";
+    
+            changepanle = questPanle.GetChild(1).GetChild(0);
+            changepanle.SetDialogVariableInt( "panleSvalue", panleSvalue);
+            changepanle.SetDialogVariableInt( "panleEvalue", panleEvalue);
+            changepanle.text = $.Localize( text , changepanle)
+        
+    });
 })();
--- scripts/vscripts/skill/00_boss_hpdar.lua
+++ scripts/vscripts/skill/00_boss_hpdar.lua
@@ -1,34 +1,34 @@
 function OnCreated(event)
-    Timers:CreateTimer(0.1, function()
-
-	--print("kkkkkkkkksss..1。。")
         local caster = event.target
-        if caster ~= nil then
-            if not caster:IsIllusion() then
-                CustomGameEventManager:Send_ServerToAllClients( "createhp", {name="hp_bar",text = "#HPBar", svalue = caster:GetHealth(),evalue=caster:GetMaxHealth()})
-                CustomGameEventManager:Send_ServerToAllClients( "refreshhpdata", {name="hp_bar",text = "#HPBar", svalue = caster:GetHealth(),evalue=caster:GetMaxHealth()})
-            end
+        if caster and not caster:IsIllusion() then
+            sentmes = {
+                name   = "hp_bar",
+                text   = "#HPBar", 
+                svalue = caster:GetHealth(),
+                evalue = caster:GetMaxHealth()
+            }
+            CustomGameEventManager:Send_ServerToAllClients( "createhp", sentmes )
+            CustomGameEventManager:Send_ServerToAllClients( "refreshhpdata", sentmes )
         end
-    end)
 end
 
 function OnDestroy(event)
     local caster = event.target
-    if caster ~= nil then
-        if not caster:IsIllusion() then
-            CustomGameEventManager:Send_ServerToAllClients( "removehppui", {name="hp_bar"})
-        end
+    if  caster and not caster:IsIllusion() then
+        CustomGameEventManager:Send_ServerToAllClients( "removehppui", {name="hp_bar"})
     end
 end
 
 function OnIntervalThink(event)
-	--print("kkkkkkkkksss..2。。")
     local caster = event.target
-    if caster ~= nil then
-        if not caster:IsIllusion() then
-            CustomGameEventManager:Send_ServerToAllClients( "refreshhpdata", {name="hp_bar",text = "#HPBar", svalue = caster:GetHealth(),evalue=caster:GetMaxHealth()})
-            
-        end
+    if caster and not caster:IsIllusion() then
+        sentmes = {
+            name   = "hp_bar",
+            text   = "#HPBar", 
+            svalue = caster:GetHealth(),
+            evalue = caster:GetMaxHealth()
+        }
+        CustomGameEventManager:Send_ServerToAllClients( "refreshhpdata", sentmes)
     end
 end
 --------------------------------上面的是BOSS血条---下面是真视-----------------------------------------------------------------------
